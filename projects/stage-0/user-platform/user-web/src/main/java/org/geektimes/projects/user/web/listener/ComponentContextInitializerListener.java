de08082ee06f458ec981cb817d329548740c46e6
charleswu <2471731988@qq.com>
2021-03-15 00:18:40 +0800

    update 02

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -2,15 +2,9 @@ package org.geektimes.projects.user.web.listener;
 
 import org.geektimes.projects.user.context.ComponentContext;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  * 初始化
@@ -23,9 +17,6 @@ public class ComponentContextInitializerListener implements ServletContextListen
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         this.servletContext = servletContextEvent.getServletContext();
-//        Connection connection = getConnection();
-//        servletContext.log("获取 JNDI End!");
-
         ComponentContext componentContext = new ComponentContext();
         componentContext.init(servletContext);
     }
@@ -35,24 +26,4 @@ public class ComponentContextInitializerListener implements ServletContextListen
         ComponentContext componentContext = ComponentContext.getInstance();
         componentContext.destroy();
     }
-
-//    private Connection getConnection() {
-//        servletContext.log("进入 getConnection()!");
-//        Connection connection = null;
-//        Context context = null;
-//
-//        try {
-//            context = new InitialContext();
-//            Context envContext = (Context) context.lookup("java:comp/env");
-//            DataSource dataSource = (DataSource) envContext.lookup("jdbc/UserPlatformDB");
-//            connection = dataSource.getConnection();
-//            servletContext.log("connection = dataSource.getConnection(): " + connection);
-//        } catch (NamingException | SQLException e) {
-//            servletContext.log(e.getMessage(), e);
-//        }
-//        if (connection != null) {
-//            servletContext.log("获取 JNDI 数据库连接成功!");
-//        }
-//        return connection;
-//    }
 }
